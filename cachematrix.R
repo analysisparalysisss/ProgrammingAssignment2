9322bce61f249fbeffc2efe9f3bc61919138901c
Nadya <analysis.paralysisss>
2015-05-12 18:51:34 +0300

    first function completed

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,24 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## The first function, makeCashMatrix creates a special "vector", which is really a list containing a function to
+set the value of the matrix
+get the value of the matrix
+set the value of the matrix
+get the value of the matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+          m <- NULL
+          set <- function(y) {
+                      x <<- y
+                      m <<- NULL
+          }
+          get <- function() x
+          setmatrix <- function(solve) m <<- solve
+          getmatrix <- function() m
+          list(set = set, get = get,
+               setmatrix = setmatrix,
+               getmatrix = getmatrix)
 }
 
 
